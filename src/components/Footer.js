23dce37d4fe423e3f0990581a7411111b0f323fd
Mariya Goncharenko <Mari.G97@yandex.com>
2023-03-09 22:18:12 +0200

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright"> ©2023. Мария Гончаренко</p>
-    </footer>
-  );
-}
-
-export default Footer;
